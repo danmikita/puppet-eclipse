27bd91c8699e2ea18321f858442eb202f84d9672
Dan Mikita <danmikita@gmail.com>
2014-04-30 09:19:12 -0400

    Update jee.pp

--- manifests/jee.pp
+++ manifests/jee.pp
@@ -5,7 +5,7 @@
 #   include eclipse::jee
 class eclipse::jee {
 
-  package { 'Eclipse-JEE-Kepler-SR1':
+  package { 'Eclipse-JEE-Kepler-SR2':
     provider => 'compressed_app',
     source   => 'http://eclipse.mirror.triple-it.nl/technology/epp/downloads/release/kepler/SR2/eclipse-jee-kepler-SR2-macosx-cocoa-x86_64.tar.gz',
   }
